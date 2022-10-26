eb8587d17e2ffbbf72c8b97cb80a98318139597d
Sebastian Sulbaran <sebastian.sulbaran@hotmail.com>
2022-10-26 00:02:50 -0300

    polimorphic

--- database/migrations/2022_10_26_025412_create_comments_table.php
+++ database/migrations/2022_10_26_025412_create_comments_table.php
@@ -16,6 +16,8 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->morphs('parent');
         });
     }
 
