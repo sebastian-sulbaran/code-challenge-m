c6b81099475001211fafe49fa574aa72bb696cea
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-10-27 03:16:32 +0000

    batches and request modifications (migrations)

---
+++ database/migrations/2022_10_26_325013_create_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            //$table->foreignId('user_id')->constrained('users');
+            $table->string('job_batch_id')->nullable();
+            $table->string('city')->nullable();
+
+            $table->json('response')->nullable();
+            // $table->foreignId('batch_id')->constrained('job_batches');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
