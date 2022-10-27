e5fffc679d1917df1ba53c04413534836ba20414
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-10-27 03:14:59 +0000

    changing trait for repository (guzzle)

--- app/Http/Controllers/WeatherRequestController.php
+++ app/Http/Controllers/WeatherRequestController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Jobs\WeatherStatus;
+use App\Models\WeatherRequest;
+use Illuminate\Support\Facades\Bus;
 class WeatherRequestController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
     }
@@ -20,18 +21,34 @@ class WeatherRequestController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $response = []; 
+        $request->validate([
+            'city' => 'required|string'
+        ]);
+        
+        $user = $request->user();
+
+        $weather_request = new WeatherRequest;
+
+        $weather_request->user_id = $user ? $user->id : null; 
+        $weather_request->city = $request->city;
+        $weather_request->save();
+        
+        $transaction = WeatherStatus::dispatch($weather_request, $request->city); //implementar con batch
+
+        $response['data']['id'] = $weather_request->id;
+
+        //$batch->finished();
+        return response()->json($response,200);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
@@ -43,7 +60,6 @@ class WeatherRequestController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
