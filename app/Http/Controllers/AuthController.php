9788cd3547e01e655a24a13a5afa3514f5945d43
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-10-28 00:06:44 +0000

    auth middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,17 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
     public function login(Request $request) {
         $request->validate([
             'email' => 'string|email|required',
