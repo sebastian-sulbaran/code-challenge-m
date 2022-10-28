386fba4e0b9a5dca60390de1e5774086beb598d5
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-10-28 00:06:19 +0000

    get user info function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,4 +62,9 @@ class AuthController extends Controller
         }
     }
 
+    public function me(Request $request)
+    {
+        $user = $request->user();
+        return response()->json($user->toArray(),200);
+    }
 }
