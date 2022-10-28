3faa84cf9eabc41a5ac5f9d0d28eab58070412e9
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-10-28 03:07:12 +0000

    implementing morph relations on comment and requests

--- app/Models/WeatherRequest.php
+++ app/Models/WeatherRequest.php
@@ -15,4 +15,9 @@ class WeatherRequest extends Model
     {
         return $this->belongsTo(User::class, "user_id", "id");
     }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'parent');
+    }
 }
